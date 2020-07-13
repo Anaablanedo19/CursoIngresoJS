c22191796ffdb65df0cdc17399a00a9649eb6710
Anaablanedo19 <ablanedo29@gmail.com>
2020-07-13 20:32:58 -0300

    jsEntradaSalida-04.js

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,10 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	var nombre;
+	nombre = prompt('Ingrese su nombre');
+	txtIdNombre.value=nombre;  // Si lo pongo al reves da igual al tres
+	//document.getElementById('txtIdNombre').value=nombre;
+    alert(txtIdNombre.value);
 }
 
