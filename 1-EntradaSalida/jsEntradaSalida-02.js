0af17adf91bb09a65d2a8e1b31ef5c02516a8edd
Anaablanedo19 <ablanedo29@gmail.com>
2020-07-13 06:39:05 -0300

    jsEntradaSalida-02.js

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,22 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+  var nombre; //unica
+  nombre = prompt("Please enter your name");
+  alert("El nombre ingresado es: "+nombre);
+  //alert(nombre);
+
+  //test 
+  /* nombre='Maria';
+  alert(nombre); */
+
+  //errores
+  /*
+  nombre=Maria; // variable sin definir
+  alert(Nombre); // variable sin definir
+  alert(maria); // variable sin definir
+  */
+
+	
 }
 
