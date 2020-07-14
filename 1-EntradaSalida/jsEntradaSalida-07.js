267c38ec6e9c3f02e4a849c9feca705d4e6c8263
Anaablanedo19 <ablanedo29@gmail.com>
2020-07-13 21:11:19 -0300

    EntradaSalida-07.js

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,39 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	var numeroUno = parseInt(txtIdNumeroUno.value);
+	var numeroDos = parseInt(txtIdNumeroDos.value);
+	 
+    var resultado = numeroUno+numeroDos
+	alert("La suma es "+ resultado);	
 }
 
 function restar()
 {
+	var numeroUno = parseInt(txtIdNumeroUno.value);
+	var numeroDos = parseInt(txtIdNumeroDos.value);
+	 
+    var resultado = numeroUno-numeroDos
+	alert("La resta es "+ resultado);
 	
 }
 
 function multiplicar()
 { 
+	var numeroUno = parseInt(txtIdNumeroUno.value);
+	var numeroDos = parseInt(txtIdNumeroDos.value);
+	 
+    var resultado = numeroUno*numeroDos
+	alert("La multiplicación es "+ resultado);
 	
 }
 
 function dividir()
 {
-	
+	var numeroUno = parseInt(txtIdNumeroUno.value);
+	var numeroDos = parseInt(txtIdNumeroDos.value);
+	 
+    var resultado = numeroUno/numeroDos
+	alert("La division es "+ resultado);
 }
 
