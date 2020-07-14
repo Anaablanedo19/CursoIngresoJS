e46a41b6c31857191c7cf2e230bddac087b7c40f
Anaablanedo19 <ablanedo29@gmail.com>
2020-07-13 22:12:38 -0300

    jsEntradaSalida-10.js

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,10 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	var importe= parseInt(txtIdImporte.value);
+	var resultado= txtIdResultado.value;
+	
+	var resultado= importe * 0.75;
+	txtIdResultado.value= resultado;
+	
 }
